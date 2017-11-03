3a336777f8ad2809e1834fcd22bb23096774c99d
wscats <2587575267@qq.com>
2017-11-03 11:07:34 +0800

    第五天下午

---
+++ day5/4.loading/components/xheader.js
@@ -0,0 +1,5 @@
+Vue.component("xheader",{
+	template:`
+		<header>日日头条</header>
+	`
+})
---
+++ day5/4.loading/components/xloading.js
@@ -0,0 +1,23 @@
+Vue.component("xloading",{
+	template:`
+		<div id="loadingToast" :style="{'display':bool?'block':'none'}">
+		    <div class="weui-mask_transparent"></div>
+		    <div class="weui-toast">
+		        <i class="weui-loading weui-icon_toast"></i>
+		        <p class="weui-toast__content">数据加载中</p>
+		    </div>
+		</div>
+	`,
+	data:function(){
+		return {
+			bool: false
+		}
+	},
+	mounted:function(){
+		var self = this
+		bus.$on("showLoading",function(status){
+			console.log(status)
+			self.bool = status
+		})
+	}
+})
